d8005a439dde85b5c7e2ef7664475cc35c5938aa
Max Kalika <max@topsy.com>
2013-01-09 00:07:51 -0800

    Some cleanup.

--- src/callbacks.c
+++ src/callbacks.c
@@ -49,18 +49,10 @@ int nurly_service_check(int event_type, void* data) {
         return NEB_ERROR;
     }
 
-//    nurly_log("host_name:           %s", service_data->host_name);
-//    nurly_log("service_description: %s", service_data->service_description);
-//    nurly_log("service_type:        %d", service_data->check_type);
-//    nurly_log("service_options:     %d", CHECK_OPTION_NONE);
-//    nurly_log("scheduled_check:     %d", CHECK_OPTION_NONE);
-//    nurly_log("reschedule_check:    %d", CHECK_OPTION_NONE);
-//    nurly_log("latency:             %f", service_data->latency);
-//    nurly_log("start_time:          %lu.%lu", service_data->start_time.tv_sec, service_data->start_time.tv_usec);
-    check_data->host    = strdup(service_data->host_name);
-    check_data->service = strdup(service_data->service_description);
-    check_data->command = strdup(service_data->command_line);
-    check_data->latency = service_data->latency;
+    check_data->host       = strdup(service_data->host_name);
+    check_data->service    = strdup(service_data->service_description);
+    check_data->command    = strdup(service_data->command_line);
+    check_data->latency    = service_data->latency;
     check_data->start_time = service_data->start_time;
 
     nurly_queue_put(&nurly_work_q, (void*)check_data);
