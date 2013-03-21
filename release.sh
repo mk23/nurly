7d063a99e77b642050339206ba682d4e83b3c17b
Max Kalika <max@topsy.com>
2013-03-20 19:11:36 -0700

    Pass extra flags (i.e. --minor, --major, etc.) to release.py.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $(readlink -e $0))
-/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e configure.ac 'nurly-module, {version},' -e server/nurly/version.py 'VERSION = "{version}"'
+/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e configure.ac 'nurly-module, {version},' -e server/nurly/version.py 'VERSION = "{version}"' $@
