60a92e4cf29c3d314467cf611d7e5d45e23d2374
Max Kalika <max@topsy.com>
2013-04-24 23:10:04 -0700

    Update links in docs when cutting a release.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $(readlink -e $0))
-/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e configure.ac 'nurly-module, {version},' -e server/httpd.py "VERSION = '{version}'" $@
+/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e configure.ac 'nurly-module, {version},' -e server/httpd.py "VERSION = '{version}'" -e README.md 'nurly-module-{version}.tar.gz' -e README.md 'nurly-server-{version}.tar.gz' $@
