2f9257a9ba8fd98190b1d2491ba7bf866e907ade
Max Kalika <mkalika@apple.com>
2015-11-20 14:09:42 -0800

    Update release driver with automatic pushing and issue extracting.

--- release.sh
+++ release.sh
@@ -1,4 +1,38 @@
 #!/bin/bash -e
 
-cd $(dirname $(readlink -e $0))
-/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e configure.ac 'nurly-module, {version},' -e server/httpd.py "VERSION = '{version}'" -e README.md 'nurly-module-{version}.tar.gz' -e README.md 'nurly-server-{version}.tar.gz' $@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "${BRANCH}" = "HEAD" ] ; then
+	echo "Cannot release on detached HEAD.  Please switch to a branch."
+	exit 1
+fi
+
+read -p "Would you like to commit ${BRANCH} branch? [y/N] " -r
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+	COMMIT=--commit
+fi
+
+echo
+(
+	/usr/bin/curl -L -s https://raw.github.com/mk23/sandbox/master/misc/release.py ||
+	echo 'raise Exception("unable to load release.py")'
+) |
+	exec /usr/bin/env python2.7 - ${COMMIT} --release stable \
+		-x '#(\d+)' 'Issue' 'https://github.com/mk23/nurly/issues/{item}' \
+		-e configure.ac 'nurly-module, {version},' \
+		-e server/httpd.py "VERSION = '{version}'" \
+		-e README.md 'nurly-module-{version}.tar.gz' \
+		-e README.md 'nurly-server-{version}.tar.gz' \
+		"$@"
+
+if [ -n "${COMMIT}" ] ; then
+	echo
+	TAG=$(git describe --abbrev=0 --tags)
+	echo "Created tag ${TAG}"
+
+	for REMOTE in $(git remote -v | cut -f1 | uniq) ; do
+		read -p "Would you like to push to ${REMOTE}? [y/N] " -r
+		if [[ $REPLY =~ ^[Yy]$ ]] ; then
+		    git push "${REMOTE}" "${BRANCH}" "${TAG}"
+		fi
+	done
+fi
